51ce7b831934dcfe08f45a21b3004739a6900639
chigagobulls95 <chigagobulls95@users.noreply.github.com>
2015-03-07 20:05:33 +0000

    Added test

---
+++ tests/test.py
@@ -0,0 +1,34 @@
+import subprocess
+import os
+import time
+from retrying import retry
+
+@retry(stop_max_delay=60000, wait_fixed=5000)
+def indexData():
+  os.system("curl -X 'POST' -H 'Content-Type:application/json' --retry 30 --retry-delay 5 -d @wikipedia_index_task.json 192.168.59.103:8085/druid/indexer/v1/task")
+  print('Indexing data!')
+
+@retry(stop_max_delay=120000, wait_fixed=5000)
+def queryData():
+  result = os.popen("curl -X 'POST' -H 'content-type: application/json' '192.168.59.103:8082/druid/v2/?pretty' --retry 30 --retry-delay 5 -d @time_bound_query.json").read()
+  if '"maxTime" : "2013-08-31T12:41:27.000Z"' not in result:
+    raise Exception('Response to query was incorrect')
+
+os.chdir('..')
+
+print('Building Docker images \n')
+os.system('./build.sh')
+
+print('Starting up docker images \n')
+os.system('docker-compose up -d')
+
+time.sleep(30)
+
+os.chdir('tests')
+
+indexData()
+
+queryData()
+
+
+
