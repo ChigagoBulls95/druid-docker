671ac7e4c166b553ae0302c28c26b30469c1cca6
chigagobulls95 <chigagobulls95@users.noreply.github.com>
2015-03-07 04:13:38 +0000

    Added wikipedia example from druid site

---
+++ examples/indexing/index.sh
@@ -0,0 +1,3 @@
+curl -X 'POST' -H 'Content-Type:application/json' -d @wikipedia_index_task.json 192.168.59.103:8085/druid/indexer/v1/task
+
+
---
+++ examples/indexing/query.sh
@@ -0,0 +1,1 @@
+curl -X POST "192.168.59.103:8082/druid/v2/?pretty" -H 'content-type: application/json' -d @time_bound_query.json
