b1e6734db1045b112afeb0a9dff273e828fbf30a
chigagobulls95 <chigagobulls95@users.noreply.github.com>
2015-03-08 01:37:20 +0000

    Made logs print out at the end to make debugging remote builds easier

--- tests/test.py
+++ tests/test.py
@@ -3,11 +3,16 @@ import os
 import time
 from retrying import retry
 
+def printDockerComposeLogs():
+  os.system('docker-compose logs &')
+
+
 @retry(stop_max_delay=60000, wait_fixed=5000)
 def indexData():
   result = os.popen("curl -X 'POST' -H 'Content-Type:application/json' --retry 30 --retry-delay 5 -d @wikipedia_index_task.json 192.168.59.103:8085/druid/indexer/v1/task").read()
   if 'task' not in result:
     raise Exception('Response to query was incorrect')
+
   print('Indexing data!')
 
 @retry(stop_max_delay=120000, wait_fixed=5000)
@@ -15,6 +20,8 @@ def queryData():
   result = os.popen("curl -X 'POST' -H 'content-type: application/json' '192.168.59.103:8082/druid/v2/?pretty' --retry 30 --retry-delay 5 -d @time_bound_query.json").read()
   if '"maxTime" : "2013-08-31T12:41:27.000Z"' not in result:
     raise Exception('Response to query was incorrect')
+
+
 
 os.chdir('..')
 
@@ -28,9 +35,11 @@ time.sleep(30)
 
 os.chdir('tests')
 
-indexData()
-
-queryData()
-
+try:
+  indexData()
+  queryData()
+except:
+  os.chdir('..')
+  printDockerComposeLogs()
 
 
