142807faa7632ddce21477459e1b146789474bc0
chigagobulls95 <chigagobulls95@users.noreply.github.com>
2015-03-08 03:43:37 +0000

    fixed java opts to allow dynamic variables not set at docker build time

--- base/common-druid.sh
+++ base/common-druid.sh
@@ -4,6 +4,8 @@ druid_config_add(){
   echo -e -n "-D$1 "
 }
 
+echo -e -n "-server -Duser.timezone=UTC -Dfile.encoding=UTF-8 -Xmx$JAVA_MAX_HEAP -Xms$JAVA_MIN_HEAP -XX:MaxDirectMemorySize=$JAVA_MAX_DIRECT_MEMORY "
+
 druid_config_add "druid.host=$(ip addr | grep 'eth0' | awk '{print $2}' | cut -f1  -d'/' | tail -1)"
 
 druid_config_add "druid.port=$DRUID_PORT"
